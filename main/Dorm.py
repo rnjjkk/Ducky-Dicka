52964631aa4ac3cc5984ceb2f3f60e18141e3a36
rnjjkk <jamesnaritnk@gmail.com>
2026-02-28 18:50:37 +0700

    add Dorm.py + class Dorm

---
+++ main/Dorm.py
@@ -0,0 +1,8 @@
+class Dorm:
+    def __init__(self, name: str):
+        self.__name: str = name
+        self.__buildings: list = []
+        self.__residents: list = []
+        self.__employees: list = []
+        self.__technician: list = []
+        self.__cleaner: list = []
