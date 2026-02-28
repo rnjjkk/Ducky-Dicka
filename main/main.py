09558af596b7437564be149a58bd570240b3d5b1
rnjjkk <jamesnaritnk@gmail.com>
2026-02-28 18:50:56 +0700

    import Dorm to main.py

--- main/main.py
+++ main/main.py
@@ -1,3 +1,9 @@
-print("test")
-print("Hello World")
-print("Tonson")
+from Dorm import Dorm
+
+if __name__ == "__main__":
+    print("test")
+    print("Hello World")
+    print("Tonson")
+
+    ducka = Dorm("ducka")
+    print(ducka)
