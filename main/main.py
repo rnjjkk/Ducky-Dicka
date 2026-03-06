4b1d510a8a3c4bbc22e68f8545b55cbe751dd2c0
rnjjkk <jamesnaritnk@gmail.com>
2026-03-06 22:59:27 +0700

    add app, add main req api

--- main/main.py
+++ main/main.py
@@ -1,6 +1,16 @@
 from models.dorm import Dorm
 from fastmcp import FastMCP
 
+dorm = Dorm("Ducka")
+
+app = FastMCP()
+
+@app.post("/request-maintenance")
+async def request_maintenance(resident_id, room_id, issue_category):
+    res = dorm.request_maintenance(resident_id, room_id, issue_category)
+    return res
+
+
 if __name__ == "__main__":
     print("test")
     print("Hello World")
