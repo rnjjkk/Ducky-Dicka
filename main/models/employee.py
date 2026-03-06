347950c8e18438d28cd971d9fb7efa20c2077270
rnjjkk <jamesnaritnk@gmail.com>
2026-03-06 23:16:05 +0700

    add fid property to employee

--- main/models/employee.py
+++ main/models/employee.py
@@ -7,5 +7,10 @@ class Employee:
         self.__id = Employee.ID
         self.__date_create = datetime.now()
         self.__name = name
+        # self.__status = None
 
         Employee.ID += 1
+
+    @property
+    def fid(self):
+        return f"EM-{self.__date_create.year}-{self.__id:04d}"
