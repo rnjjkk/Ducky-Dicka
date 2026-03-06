6ea8c0284f74561d9ae2445126045644c8bb7e17
rnjjkk <jamesnaritnk@gmail.com>
2026-03-07 00:26:13 +0700

    add technician detail

--- main/models/staff.py
+++ main/models/staff.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Staff:
     pass
 
@@ -5,7 +7,40 @@ class Cleaner(Staff):
     pass
 
 class Technician(Staff):
-    pass
+    ID = 1
+
+    def __init__(self, compabilities: list, schedule=None, current_mt=None, status="AVAILABLE"):
+        self.__id = f"TC-{datetime.now().year}-{Technician.ID:04d}"
+        self.__compabilities = compabilities
+        self.__schedule = None
+        self.__current_mt = current_mt
+        self.__status = status
+
+        Technician.ID += 1
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def compabilities(self):
+        return self.__compabilities
+
+    @property
+    def status(self):
+        return self.__status
+
+    @status.setter
+    def status(self, new_status):
+        self.__status = new_status
+
+    def assign_ticket(self, ticket):
+        self.__status = "WORKING"
+        self.__current_mt = ticket
+
+        ticket.update_maintenance_status("FINISH")
+        self.__status = "AVAILABLE"
+        return "done"
 
 class ElectricalTech(Technician):
     pass
