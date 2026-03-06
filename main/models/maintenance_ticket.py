a389e424629757996af18bda2ba66be8b5465299
rnjjkk <jamesnaritnk@gmail.com>
2026-03-07 00:05:06 +0700

    add mt detail

--- main/models/maintenance_ticket.py
+++ main/models/maintenance_ticket.py
@@ -1,6 +1,55 @@
+from datetime import datetime
+
 class MaintenanceStatus:
     pass
 
 class MaintenanceTicket:
-    def __init__(self):
-        pass
+    ID = 1
+
+    def __init__(self, reporter, room_id, issue_category, responsible_technician=None):
+        self.__id = MaintenanceTicket.ID
+        self.__reporter = reporter
+        self.__room_id = room_id
+        self.__issue_category = issue_category
+        self.__report_time = datetime.now()
+        self.__responsible_technician = responsible_technician
+        self.__approve_employee = None
+        self.__evidence_before_after = None
+        self.__status = "IDLE"
+
+        MaintenanceTicket.ID += 1
+
+    @property
+    def fid(self):
+        return f"MT-{self.__report_time.strftime("%Y%m%d")}-building-{MaintenanceTicket.ID:04d}"
+
+    @property
+    def reporter(self):
+        return self.__reporter
+
+    @reporter.setter
+    def reporter(self, reporter):
+        self.__reporter = reporter
+
+    @property
+    def room_id(self):
+        return self.__room_id
+
+    @property
+    def responsible_technician(self):
+        return self.__responsible_technician
+
+    @property
+    def status(self):
+        return self.__status
+
+    @status.setter
+    def status(self, status):
+        self.__status = status
+
+    def update_maintenance_status(self, status):
+        self.__status = status
+
+    def approve_maintenance(self, employee, status):
+        self.__approve_employee = employee.id
+        self.__status = status
