08f799e8b5333637b8a0a4bb2023fd1e414eade5
rnjjkk <jamesnaritnk@gmail.com>
2026-03-07 19:30:56 +0700

    add change contract api

--- main/main.py
+++ main/main.py
@@ -8,6 +8,29 @@ dorm = Dorm("Ducka")
 
 app = FastAPI()
 
+class ChangeContractRequest(BaseModel):
+    residentId: str
+    currentLeaseContractId: str
+    targetRoomId: str
+    moveDate: str
+
+"""
+{
+  "residentId": "1",
+  "currentLeaseContractId": "1",
+  "targetRoomId": "RM-STUDIO-A01-02-0001",
+  "moveDate": "2026-2-27"
+}
+"""
+
+@app.post("/change-contract")
+async def change_lease_contract(request: ChangeContractRequest):
+    return dorm.change_lease_contract(request.residentId,
+                                      request.currentLeaseContractId,
+                                      request.targetRoomId,
+                                      request.moveDate
+                                      )
+
 @app.post("/request-maintenance")
 async def request_maintenance(resident_id, room_id, issue_category):
     res = dorm.request_maintenance(resident_id, room_id, issue_category)
