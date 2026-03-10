17dd5f9f6e22d56adc7fdb8984fff946f7a24114
rnjjkk <jamesnaritnk@gmail.com>
2026-03-10 22:40:40 +0700

    fix name

--- DormiKa/main.py
+++ DormiKa/main.py
@@ -131,14 +131,14 @@ async def startup_event():
 class ChangeContractRequest(BaseModel):
     residentId: str = Field(..., example="RS-0001")
     currentLeaseContractId: str = Field(..., example="LC-0001")
-    targetRoomId: str = Field(..., example="RM-STUDIO-A01-01-0001")
+    targetRoomId: str = Field(..., example="RM-0001")
     moveDate: str = Field(..., example="2026-2-27")
 
 """
 {
   "residentId": "RS-0001",
   "currentLeaseContractId": "LC-0001",
-  "targetRoomId": "RM-STUDIO-A01-02-0001",
+  "targetRoomId": "RM-0001",
   "moveDate": "2026-2-27"
 }
 """
