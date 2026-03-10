d0ac5e40896fefc2dd3b494044d6c3c252f3c8b5
rnjjkk <jamesnaritnk@gmail.com>
2026-03-11 00:06:29 +0700

    add property to invoice

--- DormiKa/models/invoice.py
+++ DormiKa/models/invoice.py
@@ -11,10 +11,18 @@ class Invoice:
     ID = 1
     
     def __init__(self, id, type, room_id, amount, status):
-        self.__id = Invoice.ID
+        self.__id = f"INV-{Invoice.ID:04d}"
         self.__type = type
         self.__room_id = room_id
         self.__amount = amount
         self.__status = status
 
         Invoice.ID += 1
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def amount(self):
+        return self.__amount
