83625619d3a91346a3c7182fafbdadf488ef6d91
Pawat49 <pawattonson2549@gmail.com>
2026-03-10 23:47:27 +0700

    create class CleaningTicket

---
+++ DormiKa/models/cleaning_ticket.py
@@ -0,0 +1,6 @@
+class CleaningTicket:
+    def __init__(self, ticket_id, room_id, resident_id, status):
+        self.ticket_id = ticket_id
+        self.room_id = room_id
+        self.resident_id = resident_id
+        self.status = status
