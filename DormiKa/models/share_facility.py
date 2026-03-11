45116dbdcab0071f28a543e68d49f2cc14fc24dd
Pawat49 <pawattonson2549@gmail.com>
2026-03-11 08:14:28 +0700

    add arttibute to class ShareFacility

--- DormiKa/models/share_facility.py
+++ DormiKa/models/share_facility.py
@@ -1,9 +1,41 @@
+from .enum import *
 class ShareFacility:
+    ID = 1
     def __init__(self):
-        pass
+        self.__id = f"SHARE-{ShareFacility.ID:04d}"
+        self.__status = ShareFacilityStatus.AVAILABLE
+        self.__facility_log = []
+        ShareFacility.ID += 1
+    # getter attribute ShareFacility
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def status(self):
+        return self.__status
+    
+    @property
+    def facility_log(self):
+        return self.__facility_log
+    
+class WashingMachine(ShareFacility):
+    def __init__(self):
+        super().__init__()
+        self.__cost = 50
+    
+    # getter attribute WashingMachine
+    @property
+    def cost(self):
+        return self.__cost
 
-class WashingMachine:
-    pass
+class MeetingRoom(ShareFacility):
+    def __init__(self):
+        super().__init__()
+        self.__cost = 100
 
-class MeetingRoom:
-    pass
+    # getter attribute MeetingRoom
+    @property
+    def cost(self):
+        return self.__cost
+
