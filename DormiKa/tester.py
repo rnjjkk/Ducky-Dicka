6646a54aeab4db0f32f6e28e98a1ab4d4c8bce71
rnjjkk <jamesnaritnk@gmail.com>
2026-03-13 10:50:24 +0700

    add mock data

--- DormiKa/tester.py
+++ DormiKa/tester.py
@@ -10,22 +10,25 @@ from models.enum import *
 
 def init_mock_data():
     global dorm
-    dorm = Dorm("DormiKa")
+    dorm = Dorm("========== DormiKa ==========")
+    print(dorm.name)
 
     building = Building(floor_count=5, zone="A")
     dorm.add_building(building)
+    print(f"Building ID: {building.id}\n")
 
     rooms = [
-        Room(building, 1, RoomType.STUDIO_ROOM, RoomStatus.AVAILABLE, rental=6700),
-        Room(building, 2, RoomType.STUDIO_ROOM, RoomStatus.AVAILABLE, rental=6700),
+        Room(building, 1, RoomType.STUDIO_ROOM, RoomStatus.AVAILABLE),
+        Room(building, 2, RoomType.STUDIO_ROOM, RoomStatus.AVAILABLE),
 
-        Room(building, 3, RoomType.STANDARD_ROOM, RoomStatus.AVAILABLE, rental=8200),
-        Room(building, 4, RoomType.STANDARD_ROOM, RoomStatus.AVAILABLE, rental=8200),
+        Room(building, 3, RoomType.STANDARD_ROOM, RoomStatus.AVAILABLE),
+        Room(building, 4, RoomType.STANDARD_ROOM, RoomStatus.AVAILABLE),
 
-        Room(building, 5, RoomType.ONE_BED_ROOM, RoomStatus.AVAILABLE, rental=10500),
+        Room(building, 5, RoomType.ONE_BED_ROOM, RoomStatus.AVAILABLE),
     ]
     for room in rooms:
-        dorm.add_room(room)
+        building.add_room(room)
+        print(f"Added Room ID: {room.id}, Type: {room.type.value}, Rent: {room.monthly_rent}\n")
 
     names = ["Alice", "Bob", "Charlie", "David", "Eve", "Kenny"]
     for i in range(len(names)):
@@ -35,12 +38,15 @@ def init_mock_data():
             phone_number=f"123-456-789{i}"
         )
         dorm.add_resident(resident)
+        print(f"Added Resident: {resident.name}".ljust(25   ), end="")
+        print(f"{resident.id}")
 
     resident = dorm.residents[0]
     room = dorm.buildings[0].rooms[0]
     contract = Contract(resident, room)
     resident.add_contract(contract)
     room.status = RoomStatus.OCCUPIED
+    print(f"\nAdded {contract.id} to {resident.id}")
 
     names = ["Harry", "Sally", "Tom", "Lucy", "Mia", "Oscar"]
     for i in range(len(names)):
@@ -48,6 +54,8 @@ def init_mock_data():
             name=names[i],
         )
         dorm.add_employee(employee)
+        print(f"Added Employee: {employee.name}".ljust(25), end="")
+        print(f"{employee.id}")
 
     names = ["John", "Jane"]
     for i in range(len(names)):
@@ -58,6 +66,8 @@ def init_mock_data():
             assigned_rooms=[]
         )
         dorm.add_cleaner(cleaner)
+        print(f"Added Cleaner: {cleaner.name}".ljust(25), end="")
+        print(f"{cleaner.id}")
 
     names = ["Mike", "Sara", "Leo"]
     technicians = [
@@ -88,6 +98,13 @@ def init_mock_data():
     ]
     for t in technicians:
         dorm.add_technician(t)
+        print(f"Added Technician: {t.name}".ljust(25), end="")
+        print(f"{t.id}")
 
-if __name__ == "__main__":
-    init_mock_data()
+"""======================================="""
+
+init_mock_data()
+
+print("")
+
+dorm.system_contract_invoice("EM-0001")
